34af69253d4f8ae05b338f62a53a7ed7f9743bc7
Joshua Martinez <joshkmartinez@gmail.com>
2019-02-16 13:39:53 -0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import { Flex } from 'rebass'
+const Index = () => (
+  <React.Fragment>
+    <Head>
+      <title>Please Help Download Now</title>
+    </Head>
+    <Flex alignItems="center" justifyContent="center">
+      <div>Please Download Now</div>
+    </Flex>
+  </React.Fragment>
+)
+
+export default Index
